8f1b3ff8fa4b4e838bd04757ba9c51383722f948
valchys <vcorrales1@eafit.edu.co>
2025-03-09 16:57:30 -0500

    fix: plural migration table

--- database/migrations/2025_02_24_194556_create_computer_table.php
+++ database/migrations/2025_02_24_194556_create_computer_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('computer', function (Blueprint $table) {
+        Schema::create('computers', function (Blueprint $table) {
             $table->id();
             $table->string('reference');
             $table->string('name');
