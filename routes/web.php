83cfcc2993fa4bb921ebb69585b2d34465ce86ba
valchys <vcorrales1@eafit.edu.co>
2025-03-10 11:40:19 -0500

    fix: route names

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
-Route::get('/PCs', 'App\Http\Controllers\ComputerController@index')->name('computer.index');
-Route::get('/PCs/create', 'App\Http\Controllers\ComputerController@create')->name('computer.create');
-Route::post('/PCs/save', 'App\Http\Controllers\ComputerController@save')->name('computer.save');
-Route::get('/PCs/{id}', 'App\Http\Controllers\ComputerController@show')->name('computer.show');
-Route::delete('/PCs/{id}/delete', 'App\Http\Controllers\ComputerController@delete')->name('computer.delete');
+Route::get('/computers', 'App\Http\Controllers\ComputerController@index')->name('computer.index');
+Route::get('/computers/create', 'App\Http\Controllers\ComputerController@create')->name('computer.create');
+Route::post('/computers/save', 'App\Http\Controllers\ComputerController@save')->name('computer.save');
+Route::get('/computers/{id}', 'App\Http\Controllers\ComputerController@show')->name('computer.show');
+Route::delete('/computers/{id}/delete', 'App\Http\Controllers\ComputerController@delete')->name('computer.delete');
